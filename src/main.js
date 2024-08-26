4588fec94f2f19a043f081dc3e255302c41bb07a
MAGIC-MAN375222 <slimetime3752@gmail.com>
2024-08-26 12:16:14 -0400

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './customHUD.js';
+import './customButton.js';
+import { someUtilityFunction } from './utils.js';
+
+// Initialization code
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Client Initialized');
+  // Initialize additional features or settings here
+});
